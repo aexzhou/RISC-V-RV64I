77370a66846fad2afbc556d27c0703d60834e65f
Alex Zhou <cosr330@gmail.com>
2024-07-08 12:16:20 -0700

    Changed registers to 32 bits

--- regfile.sv
+++ regfile.sv
@@ -1,56 +1,36 @@
-module regfile(data_in,writenum,write,readnum,clk,data_out);
-    input [15:0] data_in;
-    input [2:0] writenum, readnum;
-    input write, clk;
-    output reg [15:0] data_out;
+/*
+ * 32 bit RISC-V 32I Register Module
+ */
 
-  reg [15:0] R0 = 16'b0;
-  reg [15:0] R1 = 16'b0;
-  reg [15:0] R2 = 16'b0;
-  reg [15:0] R3 = 16'b0;
-  reg [15:0] R4 = 16'b0;
-  reg [15:0] R5 = 16'b0;
-  reg [15:0] R6 = 16'b0;
-  reg [15:0] R7 = 16'b0;
+// Each register is 32 bits
+// X0 is a zero-register 32'b0 
+// 31 other general regisers, X1-X31, can hold any registers
+
+module regfile(data_in,writenum,write,readnum,clk,data_out);
+  input      [31:0] data_in;
+
+  input       [4:0] writenum, readnum; // 5 bit read/write num to target all 32 registers (2^5)
+  input             write, clk;
+  output reg [31:0] data_out;
+
+  reg [31:0] X [0:31] = '{default:32'b0}; // Array declaration of 32 of 32-bit registers
  
   // REGISTER LOGIC/WRITING LOGIC
   always @(posedge clk) begin
     if(write) begin
-      case(writenum)
-        3'b000: R0 <= data_in;
-        3'b001: R1 <= data_in;
-        3'b010: R2 <= data_in;
-        3'b011: R3 <= data_in;
-        3'b100: R4 <= data_in;
-        3'b101: R5 <= data_in;
-        3'b110: R6 <= data_in;
-        3'b111: R7 <= data_in;
-        default: begin
-                R0 <= 16'b0;
-                R1 <= 16'b0;
-                R2 <= 16'b0;
-                R3 <= 16'b0;
-                R4 <= 16'b0;
-                R5 <= 16'b0;
-                R6 <= 16'b0;
-                R7 <= 16'b0;
-                end
-      endcase
+      if(writenum < 32 && writenum != 0) begin // Cannot write to X0 since X0 always = 0
+        X[writenum] <= data_in; // Writenum maps to the index of the specific register to be written in the array.
+      end else begin
+        X <= '{default:32'b0}; // Reset Registers if writenum is out of range.
+      end 
     end
   end
 
   always @(*)begin 
-    case(readnum)
-    3'b000: data_out = R0;
-    3'b001: data_out = R1;
-    3'b010: data_out = R2;
-    3'b011: data_out = R3;
-    3'b100: data_out = R4;
-    3'b101: data_out = R5;
-    3'b110: data_out = R6;
-    3'b111: data_out = R7;
-    default: data_out = 16'b0;
-    endcase
+    if(readnum < 32) begin
+      data_out = X[readnum]; // data_out recieves data stored in the <readnum>-th register of the register array.
+    end else begin
+      data_out = '{default:32'b0}; // data_out recieves all 0s if otherwise. 
+    end 
   end
-
 endmodule
