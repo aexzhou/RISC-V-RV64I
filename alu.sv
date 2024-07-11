25337db3f401df010029cbde19469251959fbd04
Alex Zhou <cosr330@gmail.com>
2024-07-11 15:03:42 -0700

    alu updated

--- alu.sv
+++ alu.sv
@@ -1,40 +1,42 @@
 // ALUop inputs
-// 00 -> Ain + Bin
-// 01 -> Ain - Bin
-// 10 -> Ain & Bin
-// 11 -> ~Bin
+// 4'b0000: AND
+// 4'b0001: OR
+// 4'b0010: ADD
+// 4'b0110: SUB
+
+
 
 module ALU(Ain, Bin, ALUop, out, Z);
-    input [15:0] Ain, Bin;
-    input [1:0] ALUop;
-    output reg [15:0] out;
-    output reg [2:0] Z; //Z[2] is overflow, Z[1] N, Z[0] Zero
+    input [31:0] Ain, Bin;
+    input [3:0] ALUop;
+    output reg [31:0] out;
+    output reg [2:0] Z; //Z[2] is overflow, Z[1] Negative, Z[0] Zero
 
     //ADDSUB connection
-    wire [15:0] s;
+    wire [31:0] s;
     wire ovf;
 
     AddSub addsub(
         .a(Ain),
         .b(Bin),
-        .sub(ALUop[0]),
+        .sub(ALUop[2]),
         .s(s),
         .ovf(ovf)
     );
 
     always @(*) begin
         case(ALUop)
-            2'b00: begin
+            4'b0000: out = Ain && Bin;
+            4'b0001: out = Ain || Bin;
+            4'b0010: begin
                 out = Ain + Bin;
                 Z[2] = ovf; // Overflow flag, provided by addsub
             end
-            2'b01: begin
+            4'b0110: begin
                 out = Ain - Bin;
-                Z[2] = ovf;
+                Z[2] = ovf; // Overflow flag, provided by addsub
             end
-            2'b10: out = Ain & Bin;
-            2'b11: out = ~Bin;
-            default: out = {16{1'bx}};
+            default: out = {32{1'bx}};
         endcase
 
         if(out == 0)begin   // Zero flag
@@ -43,7 +45,7 @@ module ALU(Ain, Bin, ALUop, out, Z);
             Z[0] = 1'b0;
         end
 
-        if(out[15] == 1'b1)begin    // Negative Flag
+        if(out[31] == 1'b1)begin    // Negative Flag
             Z[1] = 1'b1;
         end else begin
             Z[1] = 1'b0;
@@ -52,9 +54,9 @@ module ALU(Ain, Bin, ALUop, out, Z);
 endmodule
 
 // From Dally 10.3 and Slide Set 10, Slide 28
-// add a+b or subtract a-b, check for overflow
+// add a+b or subtract a-b, check for overflow (AddSub is only used to calculate overflow)
 module AddSub(a,b,sub,s,ovf) ;
-    parameter n = 16 ;
+    parameter n = 32 ;
     input [n-1:0] a, b ;
     input sub ;           // subtract if sub=1, otherwise add
     output [n-1:0] s ;
