97cbdde7265c4a40762d5eb14d682ed093554198
Alex Zhou <cosr330@gmail.com>
2024-07-11 12:25:22 -0700

    Update datapath_tb.sv

--- datapath_tb.sv
+++ datapath_tb.sv
@@ -1,11 +1,12 @@
 `timescale 1ps/1ps
 
 module datapath_tb;
-//Inputs to Datapath
+// Inputs to Datapath
 reg [31:0] idata; // 32-bit Instruction
 
-//Outputs from Datapath
-reg [63:0] pc;
+// Error Detection
+reg err = 1'b0; 
+
 // Instantiation as an MUT Object
 datapath MUT (
     .clk(clk),
@@ -16,8 +17,7 @@ datapath MUT (
 // Clock gen
 initial begin
     clk = 0;
-    forever #5 clk = ~clk; 
-    // Delay = 10 for full clk cycle
+    forever #5 clk = ~clk; // Delay = 10 for full clk cycle
 end
 
 //Reset 
@@ -27,13 +27,33 @@ endtask
 
 /* Datapath Testbench */
 initial begin
-/* Test 1:
+/* Test 1: 
  *
- * MOV R3, #42
- * MOV R5, #13
- * ADD R2, R5, R3
+ * addi x1, x0, #5
+ * addi x2, x0, #6
+ * add x3, x1, x2
  */
-    idata = 
+    idata = 00000000010100000000000010010011;
+    #10;
+    idata = 00000000011000000000000100010011;
+    #10;
+    idata = 00000000001000001000000110110011;
+    #10;
+
+    if(MUT.REGFILE.X3 !== 64'd11) begin
+        $display("TEST 1 FAILED"); err = 1'b1; $stop; 
+    end else begin
+        $display("TEST 1 PASSED");
+    end
+    
+    if(MUT.PC !== 64'd11) begin
+        $display("TEST 1 FAILED"); err = 1'b1; $stop; 
+    end else begin
+        $display("TEST 1 PASSED");
+    end
+
+
+
 
 
 
