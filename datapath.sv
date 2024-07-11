4b9628253eed169665abbdf0e68d979054f0d12e
Alex Zhou <cosr330@gmail.com>
2024-07-11 15:01:56 -0700

    renamed and cleaned up files

--- datapath.sv
+++ datapath.sv
@@ -1,115 +1,146 @@
-// 3    - loada
-// 4    - loadb
-// 5    - loadc
-// 6    - asel
-// 7    - bsel
-// 9    - vsel
-// 10   - loads
 module datapath(
-    //Inputs
-    //input [15:0] mdata, sximm8, sximm5,
-    //input [7:0] PC,
-    //input [2:0] writenum, readnum,
-    //input [1:0] shift, ALUop, vsel,
-    //input clk, write, asel, bsel, loada, loadb, loadc, loads,
-    //output reg [15:0] datapath_out, //this is C
-    //output reg [2:0] Z_out,
-    //input shift_ctrl
+    input clk
+    input [31:0] IM_out;
 );
-// Internal registers
-reg [15:0] data_in, data_out, in, out, sout, Ain, Bin;
-reg [15:0] regAout, regBout;
-reg [2:0] Z; //vdffed to Z_out
 
-reg [1:0] shift_ctrl_out;
+// Module instantiation/connections
 
-
-
-// Module instantiation
-regfile REGFILE(data_in, writenum, write, readnum, clk, data_out); //(1)
-ALU ALU(Ain, Bin, ALUop, out, Z); //(2)
-shifter SHIFTER(regBout, shift_ctrl_out, sout); //(8)
-
-//(3)
-reg [15:0] regAtemp;
-assign regAtemp = regAout;
-always_ff @(posedge clk)begin
-    if(loada)
-        regAout = data_out;
-    else
-        regAout = regAtemp;
-end
-/*
-vDFFE #(16) regA(
+//Program Counter Register (holds the address of the next instruction to be executed)
+wire pc_enable;
+wire [63:0] pc_in;
+wire [63:0] pc_out;
+assign pc_enable = 1'b1;
+vDFFE #(64) PC(
     .clk(clk),
-    .en(loada),
-    .in(data_out),
-    .out(regAout)
+    .en(pc_enable),
+    .in(pc_in),
+    .out(pc_out)
 );
-*/
 
-//(4)
-vDFFE #(16) regB(
+// wire [31:0] IM_out;
+// InstructionMemory IM(
+//     .address(pc_out),
+//     .instruction(IM_out)
+// );
+
+wire read_reg1 = IM_out[19:15];
+wire read_reg2 = IM_out[24:20];
+wire write_reg = IM_out[11:7];
+wire opcode = IM_out[6:0];
+wire [63:0] read_data1;
+wire [63:0] read_data2;
+regfile REGFILE(write_data,write_reg,RegWrite,read_reg1,read_reg2,clk,data_out1,data_out2);
+
+wire [1:0] ALUop;
+wire Branch, MemRead, MemtoReg, MemWrite, ALUsrc, RegWrite;
+Control CONTROL(opcode, ALUop, Branch, MemRead, MemtoReg, MemWrite, ALUsrc, RegWrite);
+
+wire [63:0] imm_out;
+ImmGen IMMGEN(IM_out, imm_out);
+
+wire [63:0] ALU_in2;
+MUX64 MUX64_REG_ALU(
+    .in0(read_data2),
+    .in1(imm_out),
+    .sel(ALUsrc),
+    .out(ALU_in2)
+);
+
+wire [63:0] ALU_out;
+wire Z;                            //WARNING: Z IS 2 BIT IN ALU, BUT TEXTBOOK USES 1 BIT FOR ZERO FLAG, UPDATE REQUIRED
+ALU ALU(read_data1, ALU_in2, ALU_control_out, ALU_out, Z);
+
+wire [3:0] ALU_control_out;
+ALUcontrol ALUCONTROL(IM_out, ALUop, ALU_control_out);
+
+wire [63:0] DM_out;
+DataMemory DM(
     .clk(clk),
-    .en(loadb),
-    .in(data_out),
-    .out(regBout)
+    .address(ALU_out),
+    .write_data(read_data2),
+    .mem_read(MemRead),
+    .mem_write(MemWrite),
+    .read_data(DM_out)
 );
 
-//(5)
-vDFFE #(16) regC(
-    .clk(clk),
-    .en(loadc),
-    .in(out),
-    .out(datapath_out)
+wire [63:0] write_data;
+MUX64 MUX64_DM(
+    .in0(ALU_out),
+    .in1(DM_out),
+    .sel(MemtoReg),
+    .out(ALU_in2)
 );
 
-//(6)
-MUX16bit selA(
-    .in0(regAout),
-    .in1(16'd0),
-    .sel(asel),
-    .out(Ain)
+wire [63:0] SLA_out;
+ShiftLeftandAdd SLA(pc_out, imm_out, SLA_out);
+
+wire [63:0] PC_plus_4;
+assign PC_plus_4 = pc_out + 4;
+
+wire MUX64_PC_sel;
+assign MUX64_PC_sel = Branch & Z[0];
+
+MUX64 MUX64_PC(
+    .in0(PC_plus_4),
+    .in1(SLA_out),
+    .sel(MUX64_PC_SEL),
+    .out(pc_in)
 );
 
-//(7)
-MUX16bit selB(
-    .in0(sout),
-    .in1(sximm5),
-    .sel(bsel),
-    .out(Bin)
+
+
+
+
+// // Instruction memory module will read a 64bit address (PC) and output the corresponding instruction (RAM)
+// module InstructionMemory (
+//     input wire [63:0] address,     // Address input
+//     output reg [31:0] instruction  // Instruction output
+// );
+
+//     // Instruction memory array (stores 1024 32-bit instructions)
+//     reg [31:0] memory [0:1023];
+
+    /*
+    // Initialize the instruction memory with some values
+    initial begin
+        $readmemh("instructions.mem", memory);  // Load instructions from a file
+    end
+    */
+
+//     // Output the instruction at the given address
+//     always @(address) begin
+//         instruction = memory[address[63:2]];    // Address is byte-aligned, so divide by 4
+//     end
+// endmodule
+
+// The data memory module supports both read and write operations
+// It has separate input ports for address, data to be written, and control signals for reading and writing
+module DataMemory (
+    input wire clk,                  // Clock signal
+    input wire [63:0] address,       // Address input
+    input wire [63:0] write_data,    // Data to write
+    input wire mem_read,             // Memory read enable
+    input wire mem_write,            // Memory write enable
+    output reg [63:0] read_data      // Data read output
 );
 
-//(9)
-MUX16bit_vsel vSEL(
-    .in0(datapath_out),
-    .in1({8'b0,PC}),
-    .in2(sximm8),
-    .in3(mdata),
-    .sel(vsel),
-    .out(data_in)
-);
+    // Data memory array (stores 1024 64-bit data)
+    reg [63:0] memory [0:1023];
 
-//(10)
-vDFFE #(3) status(
-    .clk(clk),
-    .en(loads),
-    .in(Z),
-    .out(Z_out)
-);
+    // Read data
+    always @(posedge clk) begin
+        if (mem_read) begin
+            read_data <= memory[address[63:3]];  // Address is byte-aligned, so divide by 8
+        end
+    end
 
-//SPECIAL shift operation MUX specially designed for STR operations with negative imm5
-//when ON, overide shift to 00 (ADD)
-
-always_comb begin
-    if(shift_ctrl)
-        shift_ctrl_out = 2'b00;
-    else 
-        shift_ctrl_out = shift;
-end
-
+    // Write data
+    always @(posedge clk) begin
+        if (mem_write) begin
+            memory[address[63:3]] <= write_data;  // Address is byte-aligned, so divide by 8
+        end
+    end
 endmodule
-
 
 /* Immediate Generation Unit Module
 
@@ -169,7 +200,7 @@ module ALUcontrol(i, ALUop, opout);
 endmodule
 
 // Control unit for the datapath (OPCODE --> Control Signals)
-module Control(i);
+module Control(i, ALUop, Branch, MemRead, MemtoReg, MemWrite, ALUsrc, RegWrite);
     input [31:0] i;
     output [1:0] ALUop;
     output Branch;
@@ -189,7 +220,6 @@ module Control(i);
         default: output = {8{1'bx}};
     endcase
 endmodule
-
 
 //register with load enable
 module vDFFE(clk, en, in, out);
@@ -215,23 +245,4 @@ module MUX64(in0, in1, sel, out);
     assign out = sel? in1 : in0;
 endmodule
 
-//16-bit mux for vsel
-module MUX16bit_vsel(in0,in1,in2,in3,sel,out);
-
-input [15:0] in0;
-input [15:0] in1;
-input [15:0] in2;
-input [15:0] in3;
-input [1:0] sel;
-output reg [15:0] out;
-
-always @(*) begin 
-    case(sel) 
-    2'b00: out = in0;
-    2'b01: out = in1;
-    2'b10: out = in2;
-    2'b11: out = in3;
-    default: out = 16'bxxxxxxxxxxxxxxxx;
-    endcase
-end
 endmodule
