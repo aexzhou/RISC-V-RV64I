ec2446c7fceab842608ab5317624de84b4604295
Mazda-F <mazda.farrahi@gmail.com>
2024-07-22 10:24:24 -0700

    my last changes before pipeline pull

--- alu.sv
+++ alu.sv
@@ -28,12 +28,23 @@ module ALU(in1, in2, ALUop, out, Z);
 
     always @(*) begin
         case(ALUop)
+<<<<<<< Updated upstream
             4'b0000: out = in1 & in2;
             4'b0001: out = in1 | in2;
             4'b0010: out = in1 + in2;
             4'b0110: out = in1 - in2;
             4'b0111: out = (in1<in2)? 64'd1 : 64'd0; // SLT
             4'b1100: out = ~(in1 | in2); // NOR
+=======
+            4'b0000: out = in1 && in2;
+            4'b0001: out = in1 || in2;
+            4'b0010: begin
+                out = in1 + in2;
+            end
+            4'b0110: begin
+                out = in1 - in2;
+            end
+>>>>>>> Stashed changes
             default: out = {64{1'bx}};
         endcase
 
