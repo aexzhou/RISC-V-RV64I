bd1878b2943e4cfdc9491eb1bb87c2e2f26c0f2b
aexzhou <cosr330@gmail.com>
2024-08-13 18:52:59 -0700

    finally flashes to the board

--- top_level.sv
+++ top_level.sv
@@ -1,396 +1,85 @@
 import hidden_wires_pkg::*;
 import hidden_clk_pkg::*;
 
-module top_level (
-	////////////////////////////////////
-	// FPGA Pins
-	////////////////////////////////////
+module top_level(
 
-	// Clock pins
-	CLOCK_50,
-	CLOCK2_50,
-	CLOCK3_50,
-	CLOCK4_50,
+	//////////// CLOCK //////////
+	input 		          		CLOCK2_50,
+	input 		          		CLOCK3_50,
+	input 		          		CLOCK4_50,
+	input 		          		CLOCK_50,
 
-	// ADC
-	ADC_CS_N,
-	ADC_DIN,
-	ADC_DOUT,
-	ADC_SCLK,
+	//////////// KEY //////////
+	input 		     [3:0]		KEY,
 
-	// Audio
-	AUD_ADCDAT,
-	AUD_ADCLRCK,
-	AUD_BCLK,
-	AUD_DACDAT,
-	AUD_DACLRCK,
-	AUD_XCK,
-
-	// SDRAM
-	DRAM_ADDR,
-	DRAM_BA,
-	DRAM_CAS_N,
-	DRAM_CKE,
-	DRAM_CLK,
-	DRAM_CS_N,
-	DRAM_DQ,
-	DRAM_LDQM,
-	DRAM_RAS_N,
-	DRAM_UDQM,
-	DRAM_WE_N,
-
-	// I2C Bus for Configuration of the Audio and Video-In Chips
-	FPGA_I2C_SCLK,
-	FPGA_I2C_SDAT,
-
-	// 40-Pin Headers
-	GPIO_0,
-	GPIO_1,
+	//////////// HPS //////////
+	inout 		          		HPS_CONV_USB_N,
+	output		    [14:0]		HPS_DDR3_ADDR,
+	output		     [2:0]		HPS_DDR3_BA,
+	output		          		HPS_DDR3_CAS_N,
+	output		          		HPS_DDR3_CKE,
+	output		          		HPS_DDR3_CK_N,
+	output		          		HPS_DDR3_CK_P,
+	output		          		HPS_DDR3_CS_N,
+	output		     [3:0]		HPS_DDR3_DM,
+	inout 		    [31:0]		HPS_DDR3_DQ,
+	inout 		     [3:0]		HPS_DDR3_DQS_N,
+	inout 		     [3:0]		HPS_DDR3_DQS_P,
+	output		          		HPS_DDR3_ODT,
+	output		          		HPS_DDR3_RAS_N,
+	output		          		HPS_DDR3_RESET_N,
+	input 		          		HPS_DDR3_RZQ,
+	output		          		HPS_DDR3_WE_N,
+	output		          		HPS_ENET_GTX_CLK,
+	inout 		          		HPS_ENET_INT_N,
+	output		          		HPS_ENET_MDC,
+	inout 		          		HPS_ENET_MDIO,
+	input 		          		HPS_ENET_RX_CLK,
+	input 		     [3:0]		HPS_ENET_RX_DATA,
+	input 		          		HPS_ENET_RX_DV,
+	output		     [3:0]		HPS_ENET_TX_DATA,
+	output		          		HPS_ENET_TX_EN,
+	inout 		     [3:0]		HPS_FLASH_DATA,
+	output		          		HPS_FLASH_DCLK,
+	output		          		HPS_FLASH_NCSO,
+	inout 		          		HPS_GSENSOR_INT,
+	inout 		          		HPS_I2C1_SCLK,
+	inout 		          		HPS_I2C1_SDAT,
+	inout 		          		HPS_I2C2_SCLK,
+	inout 		          		HPS_I2C2_SDAT,
+	inout 		          		HPS_I2C_CONTROL,
+	inout 		          		HPS_KEY,
+	inout 		          		HPS_LED,
+	inout 		          		HPS_LTC_GPIO,
+	output		          		HPS_SD_CLK,
+	inout 		          		HPS_SD_CMD,
+	inout 		     [3:0]		HPS_SD_DATA,
+	output		          		HPS_SPIM_CLK,
+	input 		          		HPS_SPIM_MISO,
+	output		          		HPS_SPIM_MOSI,
+	inout 		          		HPS_SPIM_SS,
+	input 		          		HPS_UART_RX,
+	output		          		HPS_UART_TX,
+	input 		          		HPS_USB_CLKOUT,
+	inout 		     [7:0]		HPS_USB_DATA,
+	input 		          		HPS_USB_DIR,
+	input 		          		HPS_USB_NXT,
+	output		          		HPS_USB_STP,
 	
-	// Seven Segment Displays
-	HEX0,
-	HEX1,
-	HEX2,
-	HEX3,
-	HEX4,
-	HEX5,
-
-	// IR
-	IRDA_RXD,
-	IRDA_TXD,
-
-	// Pushbuttons
-	KEY,
-
-	// LEDs
-	LEDR,
-
-	// PS2 Ports
-	PS2_CLK,
-	PS2_DAT,
-	
-	PS2_CLK2,
-	PS2_DAT2,
-
-	// Slider Switches
-	SW,
-
-	// Video-In
-	TD_CLK27,
-	TD_DATA,
-	TD_HS,
-	TD_RESET_N,
-	TD_VS,
-
-	// VGA
-	VGA_B,
-	VGA_BLANK_N,
-	VGA_CLK,
-	VGA_G,
-	VGA_HS,
-	VGA_R,
-	VGA_SYNC_N,
-	VGA_VS,
-
-	////////////////////////////////////
-	// HPS Pins
-	////////////////////////////////////
-	
-	// DDR3 SDRAM
-	HPS_DDR3_ADDR,
-	HPS_DDR3_BA,
-	HPS_DDR3_CAS_N,
-	HPS_DDR3_CKE,
-	HPS_DDR3_CK_N,
-	HPS_DDR3_CK_P,
-	HPS_DDR3_CS_N,
-	HPS_DDR3_DM,
-	HPS_DDR3_DQ,
-	HPS_DDR3_DQS_N,
-	HPS_DDR3_DQS_P,
-	HPS_DDR3_ODT,
-	HPS_DDR3_RAS_N,
-	HPS_DDR3_RESET_N,
-	HPS_DDR3_RZQ,
-	HPS_DDR3_WE_N,
-
-	// Ethernet
-	HPS_ENET_GTX_CLK,
-	HPS_ENET_INT_N,
-	HPS_ENET_MDC,
-	HPS_ENET_MDIO,
-	HPS_ENET_RX_CLK,
-	HPS_ENET_RX_DATA,
-	HPS_ENET_RX_DV,
-	HPS_ENET_TX_DATA,
-	HPS_ENET_TX_EN,
-
-	// Flash
-	HPS_FLASH_DATA,
-	HPS_FLASH_DCLK,
-	HPS_FLASH_NCSO,
-
-	// Accelerometer
-	HPS_GSENSOR_INT,
-		
 	// General Purpose I/O
-	HPS_GPIO,
-		
-	// I2C
-	HPS_I2C_CONTROL,
-	HPS_I2C1_SCLK,
-	HPS_I2C1_SDAT,
-	HPS_I2C2_SCLK,
-	HPS_I2C2_SDAT,
-
-	// Pushbutton
-	HPS_KEY,
-
-	// LED
-	HPS_LED,
-		
-	// SD Card
-	HPS_SD_CLK,
-	HPS_SD_CMD,
-	HPS_SD_DATA,
-
-	// SPI
-	HPS_SPIM_CLK,
-	HPS_SPIM_MISO,
-	HPS_SPIM_MOSI,
-	HPS_SPIM_SS,
-
-	// UART
-	HPS_UART_RX,
-	HPS_UART_TX,
-
-	// USB
-	HPS_CONV_USB_N,
-	HPS_USB_CLKOUT,
-	HPS_USB_DATA,
-	HPS_USB_DIR,
-	HPS_USB_NXT,
-	HPS_USB_STP
+    inout			 [1:0]	    HPS_GPIO
 );
 
-//=======================================================
-//  PARAMETER declarations
-//=======================================================
 
-
-//=======================================================
-//  PORT declarations
-//=======================================================
-
-////////////////////////////////////
-// FPGA Pins
-////////////////////////////////////
-
-// Clock pins
-input						CLOCK_50;
-input						CLOCK2_50;
-input						CLOCK3_50;
-input						CLOCK4_50;
-
-// ADC
-inout						ADC_CS_N;
-output					ADC_DIN;
-input						ADC_DOUT;
-output					ADC_SCLK;
-
-// Audio
-input						AUD_ADCDAT;
-inout						AUD_ADCLRCK;
-inout						AUD_BCLK;
-output					AUD_DACDAT;
-inout						AUD_DACLRCK;
-output					AUD_XCK;
-
-// SDRAM
-output 		[12: 0]	DRAM_ADDR;
-output		[ 1: 0]	DRAM_BA;
-output					DRAM_CAS_N;
-output					DRAM_CKE;
-output					DRAM_CLK;
-output					DRAM_CS_N;
-inout			[15: 0]	DRAM_DQ;
-output					DRAM_LDQM;
-output					DRAM_RAS_N;
-output					DRAM_UDQM;
-output					DRAM_WE_N;
-
-// I2C Bus for Configuration of the Audio and Video-In Chips
-output					FPGA_I2C_SCLK;
-inout						FPGA_I2C_SDAT;
-
-// 40-pin headers
-inout			[35: 0]	GPIO_0;
-inout			[35: 0]	GPIO_1;
-
-// Seven Segment Displays
-output		[ 6: 0]	HEX0;
-output		[ 6: 0]	HEX1;
-output		[ 6: 0]	HEX2;
-output		[ 6: 0]	HEX3;
-output		[ 6: 0]	HEX4;
-output		[ 6: 0]	HEX5;
-
-// IR
-input						IRDA_RXD;
-output					IRDA_TXD;
-
-// Pushbuttons
-input			[ 3: 0]	KEY;
-
-// LEDs
-output		[ 9: 0]	LEDR;
-
-// PS2 Ports
-inout						PS2_CLK;
-inout						PS2_DAT;
-
-inout						PS2_CLK2;
-inout						PS2_DAT2;
-
-// Slider Switches
-input			[ 9: 0]	SW;
-
-// Video-In
-input						TD_CLK27;
-input			[ 7: 0]	TD_DATA;
-input						TD_HS;
-output					TD_RESET_N;
-input						TD_VS;
-
-// VGA
-output		[ 7: 0]	VGA_B;
-output					VGA_BLANK_N;
-output					VGA_CLK;
-output		[ 7: 0]	VGA_G;
-output					VGA_HS;
-output		[ 7: 0]	VGA_R;
-output					VGA_SYNC_N;
-output					VGA_VS;
-
-
-
-////////////////////////////////////
-// HPS Pins
-////////////////////////////////////
-	
-// DDR3 SDRAM
-output		[14: 0]	HPS_DDR3_ADDR;
-output		[ 2: 0]  HPS_DDR3_BA;
-output					HPS_DDR3_CAS_N;
-output					HPS_DDR3_CKE;
-output					HPS_DDR3_CK_N;
-output					HPS_DDR3_CK_P;
-output					HPS_DDR3_CS_N;
-output		[ 3: 0]	HPS_DDR3_DM;
-inout			[31: 0]	HPS_DDR3_DQ;
-inout			[ 3: 0]	HPS_DDR3_DQS_N;
-inout			[ 3: 0]	HPS_DDR3_DQS_P;
-output					HPS_DDR3_ODT;
-output					HPS_DDR3_RAS_N;
-output					HPS_DDR3_RESET_N;
-input						HPS_DDR3_RZQ;
-output					HPS_DDR3_WE_N;
-
-// Ethernet
-output					HPS_ENET_GTX_CLK;
-inout						HPS_ENET_INT_N;
-output					HPS_ENET_MDC;
-inout						HPS_ENET_MDIO;
-input						HPS_ENET_RX_CLK;
-input			[ 3: 0]	HPS_ENET_RX_DATA;
-input						HPS_ENET_RX_DV;
-output		[ 3: 0]	HPS_ENET_TX_DATA;
-output					HPS_ENET_TX_EN;
-
-// Flash
-inout			[ 3: 0]	HPS_FLASH_DATA;
-output					HPS_FLASH_DCLK;
-output					HPS_FLASH_NCSO;
-
-// Accelerometer
-inout						HPS_GSENSOR_INT;
-
-// General Purpose I/O
-inout			[ 1: 0]	HPS_GPIO;
-
-// I2C
-inout						HPS_I2C_CONTROL;
-inout						HPS_I2C1_SCLK;
-inout						HPS_I2C1_SDAT;
-inout						HPS_I2C2_SCLK;
-inout						HPS_I2C2_SDAT;
-
-// Pushbutton
-inout						HPS_KEY;
-
-// LED
-inout						HPS_LED;
-
-// SD Card
-output					HPS_SD_CLK;
-inout						HPS_SD_CMD;
-inout			[ 3: 0]	HPS_SD_DATA;
-
-// SPI
-output					HPS_SPIM_CLK;
-input						HPS_SPIM_MISO;
-output					HPS_SPIM_MOSI;
-inout						HPS_SPIM_SS;
-
-// UART
-input						HPS_UART_RX;
-output					HPS_UART_TX;
-
-// USB
-inout						HPS_CONV_USB_N;
-input						HPS_USB_CLKOUT;
-inout			[ 7: 0]	HPS_USB_DATA;
-input						HPS_USB_DIR;
-input						HPS_USB_NXT;
-output					HPS_USB_STP;
 
 //=======================================================
 //  REG/WIRE declarations
 //=======================================================
-
-//wire			[15: 0]	hex3_hex0;
-//wire			[15: 0]	hex5_hex4;
-
-//assign HEX0 = ~hex3_hex0[ 6: 0]; // hex3_hex0[ 6: 0]; 
-//assign HEX1 = ~hex3_hex0[14: 8];
-//assign HEX2 = ~hex3_hex0[22:16];
-//assign HEX3 = ~hex3_hex0[30:24];
-//assign HEX4 = 7'b1111111;
-//assign HEX5 = 7'b1111111;
-
-//HexDigit Digit0(HEX0, hex3_hex0[3:0]);
-//HexDigit Digit1(HEX1, hex3_hex0[7:4]);
-//HexDigit Digit2(HEX2, hex3_hex0[11:8]);
-//HexDigit Digit3(HEX3, hex3_hex0[15:12]);
-
-
-/*
-Top level state machine
-
-0 - SRAM -> instruction memory **
-1 - Start Clocking the CPU until its on its last instruction
-2 - Delay 10 Clocks since the CPU is pipelined 
-3 - data memory -> SRAM
-
-*/
 wire cpu_clk;
 reg cpu_clk_src;
 assign cpu_clk = (cpu_clk_src)? CLOCK_50 : 1'b0; 
 reg cpu_rst = 0;
 cpu CPU(cpu_clk, cpu_rst);
-
-
-
 
 //=======================================================
 // HPS_to_FPGA FIFO state machine
@@ -615,12 +304,9 @@ always @(posedge CLOCK_50) begin // CLOCK_50
 	//==================================
 end // always @(posedge state_clock)
 
-
 //=======================================================
 //  Structural coding
 //=======================================================
-// From Qsys
-
 hps_fpga HPS_FPGA (
 	////////////////////////////////////
 	// FPGA Side
@@ -710,11 +396,6 @@ hps_fpga HPS_FPGA (
 	// Accelerometer
 	.hps_io_hps_io_gpio_inst_GPIO61	(HPS_GSENSOR_INT),
 
-	//.adc_sclk                        (ADC_SCLK),
-	//.adc_cs_n                        (ADC_CS_N),
-	//.adc_dout                        (ADC_DOUT),
-	//.adc_din                         (ADC_DIN),
-
 	// General Purpose I/O
 	.hps_io_hps_io_gpio_inst_GPIO40	(HPS_GPIO[0]),
 	.hps_io_hps_io_gpio_inst_GPIO41	(HPS_GPIO[1]),
@@ -765,4 +446,6 @@ hps_fpga HPS_FPGA (
 	.hps_io_hps_io_usb1_inst_DIR		(HPS_USB_DIR),
 	.hps_io_hps_io_usb1_inst_NXT		(HPS_USB_NXT)
 );
-endmodule // end top level
+
+
+endmodule
